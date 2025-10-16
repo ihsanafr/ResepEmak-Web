6c6742bf796ec665516f2e9e14542c7b4d579502
ihsan <ihsan.ahmad2404@gmail.com>
2025-10-16 22:14:04 +0700

    Feat:Make API Resource Category #16/10

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\Api\CategoryResource;
+
+class CategoryController extends Controller
+{
+    //
+    public function index()
+    {
+        $categories = Category::withCount('recipes')->get();
+        return CategoryResource::collection($categories);
+    }
+
+    public function show(Category $category)
+    {
+        $category->load('recipes');
+        return new CategoryResource($category);
+    }
+}
